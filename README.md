2e8d10c805ae7206dd8fe4e01d78795d2e8f3668
Angelo Evangelista <angeloevan.ane@gmail.com>
2022-08-31 14:27:31 -0300

    Added card

---
+++ README.md
@@ -0,0 +1,27 @@
+# Hi, Angelo here!
+
+<div align="center" height="500">
+  <img
+      align="left"
+      title="Coding"
+      src="https://raw.githubusercontent.com/angeloevangelista/angeloevangelista/master/.github/images/rounded-coding.gif" 
+      style="
+        width: 120px;
+        height: 120px;
+      "
+    />
+</div>
+
+This is my NPX card to connect with me directly via console or terminal
+
+## How to use it
+
+Just go to your terminal and run that:
+
+```sh
+npx @angeloevangelista/card
+```
+
+I will not show how to install it globally. You know, it would be a kind of weird 🧐
+
+This project was based on this [amazing project](https://github.com/carinebatista/npx_card#readme) from [Carine Batista](https://github.com/carinebatista). Thanks for the inspiration 💜.
---
+++ card.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+// Using node to execute js
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+
+const data = {
+  name: chalk.red("                          Angelo Evangelista"),
+  work: `${chalk("         FullStack Developer at")} ${chalk.yellowBright("Foursys")}`,
+
+  github: chalk.gray("https://github.com/") + chalk.green("angeloevangelista"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.green("angelo-evangelista-5474a2177"),
+  email: chalk.green("angeloevan.ane@gmail.com"),
+  skills: chalk.redBright("C#", "HTML", "CSS", "JS", "TS", "NodeJS", "ReactJs", "Angular"),
+  npx: chalk.red("npx @angeloevangelista/card"),
+
+  labelWork: ("         "),
+  labelGitHub: chalk.white.bold("        GitHub:"),
+  labelLinkedIn: chalk.white.bold("      LinkedIn:"),
+  labelEmail: chalk.white.bold("         Email:"),
+  labelSkills: chalk.blue.bold("        Skills:"),
+  labelCard: chalk.white.bold("          Card:")
+};
+
+const lines = [
+  `${data.name}`,
+  ``,
+  `${data.labelWork}  ${data.work}`,
+  ``,
+  `${data.labelGitHub}  ${data.github}`,
+  `${data.labelLinkedIn}  ${data.linkedin}`,
+  `${data.labelEmail}  ${data.email}`,
+  ``,
+  `${data.labelSkills}  ${data.skills}`,
+  ``,
+  `${data.labelCard}  ${data.npx}`,
+  ``,
+
+  `${chalk.italic("                   Pull requests are always welcome.")}`,
+  `${chalk.italic("                   Let's code together! 🚀")}`
+];
+
+const me = boxen(
+  lines.join("\n"),
+  {
+    margin: 1,
+    float: 'center',
+    padding: 1,
+    borderStyle: "round",
+    borderColor: "black",
+  }
+);
+
+console.log(me);
